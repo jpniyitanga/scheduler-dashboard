4a9fda8d389ed52266b50184ef3125933a2f96b0
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-06-19 19:25:35 -0400

    Handling state and binding 'this' keyword

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import Dashboard from "./Dashboard";
 
 class Panel extends Component {
 
   render() {
-    const {label, value } = this.props;
+    const { label, value, onSelect } = this.props;
+    
+        
 
     return (
-      <section className="dashboad_panel">
+      <section className="dashboad_panel" onClick={onSelect}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
